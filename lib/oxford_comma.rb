d82393062ed2d5e621a5ffa58f7e20c4e7f87204
Lisa Feng <lisafeng10@gmail.com>
2020-06-17 17:27:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(array)
   if array.length == 2
     array.join("and")
   elsif array.length > 2
-    
+    last = array.pop
+    array.join(", ") << "and #{last}"
+  else
+    array.join
   end
 end
